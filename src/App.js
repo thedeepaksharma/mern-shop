43c08ba865eb88b5ae9977d93ec36adeaf331481
Deepak Sharma <beingdeepak786@gmail.com>
2022-09-26 16:18:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/users" element={<UserList />} />
+          <Route exact path="/user/:userId" element={<User />} />
+          <Route exact path="/newUser" element={<NewUser />} />
+          <Route exact path="/products" element={<ProductList />} />
+          <Route exact path="/product/:productId" element={<Product />} />
+          <Route exact path="/newproduct" element={<NewProduct />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
